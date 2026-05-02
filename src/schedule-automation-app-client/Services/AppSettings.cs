95595c679dfaa0ca3b02fc66e81760c092080b33
Екатерина Ляпина <89636319125k@gmail.com>
2026-05-02 14:40:42 +0300

    fix(client): fix AppSettings

--- src/schedule-automation-app-client/Services/AppSettings.cs
+++ src/schedule-automation-app-client/Services/AppSettings.cs
@@ -2,5 +2,5 @@ namespace schedule_automation_app_client.Services;
 
 public class AppSettings
 {
-    public static string ServerBaseUrl { get; set; } = "http://localhost:5284";
+    public static string ServerBaseUrl { get; } = "http://localhost:5284";
 }
