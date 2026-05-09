6a0160d0e96654755f567344cde338bd22b3bf78
Кристина <kvdeshina@edu.hse.ru>
2026-05-09 18:32:40 +0300

    server: unit тесты для GradeCalculationService, EffortMinimizationSolver и доменных сущностей
    - GradeCalculationServiceTests: расчет текущей оценки, достижимость цели, округление 0.5, блокирующие компоненты, автомат, диапазон планов
    - EffortMinimizationSolverTests: порядок улучшения, блокирующие минимумы, граничные значения
    - DomainEntityTests: валидация Subject и GradeComponent, граничные значения, вспомогательные методы

---
+++ src/schedule-automation-app-server.Tests/TestHelpers.cs
@@ -0,0 +1,50 @@
+using schedule_automation_app_server.Domain.Entities;
+
+namespace schedule_automation_app_server.Tests;
+
+public static class TestHelpers
+{
+    public static Subject CreateSubject(
+        string name = "Тест",
+        int targetGrade = 7,
+        params (string name, double weight, int complexity, double currentGrade, bool isGraded, bool isBlocking, double minimumGrade, bool isAutoGrade, double autoGradeMinScore)[] components)
+    {
+        Subject subject = new Subject(name, targetGrade);
+
+        foreach (var c in components)
+        {
+            GradeComponent component = new GradeComponent(
+                c.name, c.weight, c.complexity, c.currentGrade, c.isGraded)
+            {
+                IsBlocking = c.isBlocking,
+                MinimumGrade = c.minimumGrade,
+                IsAutoGrade = c.isAutoGrade,
+                AutoGradeMinScore = c.autoGradeMinScore
+            };
+
+            subject.AddComponent(component);
+        }
+
+        return subject;
+    }
+
+    public static GradeComponent MakeComponent(
+        string name = "Компонент",
+        double weight = 50,
+        int complexity = 5,
+        double currentGrade = 0,
+        bool isGraded = false,
+        bool isBlocking = false,
+        double minimumGrade = 0,
+        bool isAutoGrade = false,
+        double autoGradeMinScore = 0)
+    {
+        return new GradeComponent(name, weight, complexity, currentGrade, isGraded)
+        {
+            IsBlocking = isBlocking,
+            MinimumGrade = minimumGrade,
+            IsAutoGrade = isAutoGrade,
+            AutoGradeMinScore = autoGradeMinScore
+        };
+    }
+}
